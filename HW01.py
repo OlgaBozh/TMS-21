9ad0718ba54a427354f29992f9aac9b6cfbd3c02
OlgaBozh <94069031+OlgaBozh@users.noreply.github.com>
2021-11-11 18:33:55 +0300

    Add files via upload

---
+++ HW01.py
@@ -0,0 +1,57 @@
+#Task_1_1
+numb_1 = 3
+numb_2 = 7
+
+print(numb_1)
+print(numb_2)
+
+summ = (numb_1 + numb_2)
+print(summ)
+raznost = (numb_2 - numb_1)
+print(raznost)
+proizv = (numb_1 * numb_2)
+print (proizv)
+
+
+#Task_1_2
+y = 5
+x = 7
+print(y)
+print(x)
+otvet = (x-y)/(1+x*y)
+print (otvet)
+
+
+#Task_1_3
+rebro = 36
+print(rebro)
+v_kuba = rebro**3
+print(v_kuba)
+s_kuba = rebro**2
+print(s_kuba)
+
+
+#Task_1_4
+numb_1 = 6
+numb_2 = 8
+print(numb_1)
+print(numb_2)
+s_ar = (numb_1 + numb_2)/2
+print(s_ar)
+s_geom = (numb_2 * numb_1)**(1/2)
+print(s_geom)
+
+
+#Task_1_5
+a = 3
+b = 4
+
+print(a)
+print(b)
+
+gip = (a**2 + b**2)**(1/2)
+print(gip)
+s_tr = (a*b)/2
+print(s_tr)
+
+
